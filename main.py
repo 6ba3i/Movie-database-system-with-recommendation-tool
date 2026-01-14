e25c6fdd65210de3115889d282f756e77600dea3
6ba3i <112825897+6ba3i@users.noreply.github.com>
2026-01-14 13:25:08 +0800

    remove api key

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def login():
         password = request.form.get("password")
         if email and password:
             # Use the provided API key (consider storing it in an environment variable for production)
-            api_key = "AIzaSyBOZ8bjH-yvFQMgi1FC7nZIMJQEhxb2E3c"
+            api_key = "personal_api_key"
             url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
             payload = {
                 "email": email,
